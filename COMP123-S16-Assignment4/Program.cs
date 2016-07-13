f0ea3a5c9ab3b05e84cb70cf354ec501bccb06c0
300872772 <mamun_131@hotmail.com>
2016-07-13 00:30:30 -0300

    added all private instatnce variable

--- COMP123-S16-Assignment4/Program.cs
+++ COMP123-S16-Assignment4/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* 
+ *Author: Md Mamunur Rahman 
+ * Student ID: 300872772  
+ * 
+ * Date: July 11, 2016 
+ * Description: This program demonstrates a Dice Rolling Simulation  
+ *  
+ * Version: 0.0.1 - added all private instatnce variable
+ */
 namespace COMP123_S16_Assignment4
 {
     class Program
     {
+        private static Random _random;
+        private static int _dice;
+        private const int _defaultCount = 36000;
+        private static List<int[]> _diceCount = new List<int[]>();
+
+
+
         static void Main(string[] args)
         {
+
         }
+
+      
+
+
     }
 }
